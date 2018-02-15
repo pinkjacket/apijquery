3a8f2e25a9ae65d75afec327bd7c5ec5677aad88
James Donlan <jacketlizard@live.com>
2018-02-15 15:31:33 -0800

    api call now refactored using ES6 instead of jquery despite repo name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 var apiKey = require("./../.env").apiKey;
+// api key goes in top-level.env file not included in repo for security reasons
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      }).fail(function(error) {
-        $('.showErrors').text("There was an error processing your request. Please try again.");
-      });
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
+});
