26334b8b2f32e032a6c1a3551388f57ace29295b
James Donlan <jacketlizard@live.com>
2018-02-15 13:49:33 -0800

    code updated to use promises

--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
-      }
+      }).fail(function(error) {
+        $('.showErrors').text("There was an error processing your request. Please try again.");
+      });
     });
   });
-});
